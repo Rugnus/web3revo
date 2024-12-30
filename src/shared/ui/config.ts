a82f4af78122c4ff0295df4e46e725dbb87e107a
SYGasanov <sygasanov@get-lab.ru>
2024-12-30 19:11:34 +0300

    add ui config

---
+++ src/shared/ui/config.ts
@@ -0,0 +1,13 @@
+export const pxToRem = (px: number) =>
+  `${px / parseInt(fontSize.default, 10)}rem`;
+
+export const uiColors = {
+  primary: "#0070f3",
+  secondary: "#a294f9",
+  background: "#333333",
+  white: "#ffffff",
+};
+
+export const fontSize = {
+  default: "16px",
+};
