1e0356b2874531e80b8d4e85a564f704656e54e3
SYGasanov <sygasanov@get-lab.ru>
2024-12-31 21:10:48 +0300

    add prisma db

---
+++ src/shared/libs/prisma/migrations/20241231170507_remove_created_at/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "surname" TEXT,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
